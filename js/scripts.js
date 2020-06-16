3a549911ddec0ee82c817b7bfbbb288c118fcc35
Ben White <bwhite2140@outlook.com>
2020-06-16 14:51:13 -0700

    add bank account construct and deposit and withdrawal prototypes

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,16 @@
-$(document).ready(function() {
+function BankAccount(name, balance) {
+  this.name = name,
+  this.balance = balance
+}
 
+BankAccount.prototype.deposit = function(deposit, balance) {
+  return balance += deposit;
+}
+
+BankAccount.prototype.withdrawl = function(withdrawal, balance) {
+  return balance -= withdrawal;
+}
+
+$(document).ready(function() {
+  
 });
