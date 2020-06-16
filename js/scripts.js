de6a9522c74a1ea802a6de62245d44ccdaa45575
Ben White <bwhite2140@outlook.com>
2020-06-16 15:50:55 -0700

    add user logic to allow user to create an account with an initial deposit

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
-function BankAccount(name, balance) {
+function BankAccount(name, deposit) {
   this.name = name,
-  this.balance = balance
+  this.balance = deposit
 }
 
 BankAccount.prototype.deposit = function(deposit, balance) {
@@ -11,6 +11,17 @@ BankAccount.prototype.withdrawl = function(withdrawal, balance) {
   return balance -= withdrawal;
 }
 
+const showBalance = function(bankAccount) {
+  $("#balance").text(bankAccount.balance);
+  $("#balance").show();
+}
+
 $(document).ready(function() {
-  
+  $("#new-account").submit(function(event) {
+    event.preventDefault();
+    const name = $("input#name").val();
+    let deposit = parseInt($("input#initial-deposit").val());
+    let bankAccount = new BankAccount(name, deposit);
+    showBalance(bankAccount);
+  });
 });
